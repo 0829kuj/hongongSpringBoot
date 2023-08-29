5155386aceae7b5ea5287a474f17c5966da4746f
KEJ <0829kuj@naver>
2023-08-30 08:50:54 +0900

    day2 - 화면레이아웃적용

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,4 +1,20 @@
 package com.example.firstproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model m, String name){
+        m.addAttribute("username", name);
+        return "greetings";     // greetings.mustache 파일반환
+    }
+    @GetMapping("/bye/{name}")
+    public String seeYouNext(Model m, @PathVariable String name){
+        m.addAttribute("username", name);
+        return "goodbye";
+    }
 }
